5d006555bd84019db4da3f14f995fe7bb8eb6692
rileygramlich <rileygramlich@gmail.com>
2022-11-25 11:28:48 -0700

    set up app dirs, mongoose, and nav bar

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1/movies', {
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+	
+db.on('connected', function() {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
+
+// gotta connect to api's here I do believe
